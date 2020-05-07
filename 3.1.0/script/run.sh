485aee3ea5ad06ce6a67f0aeadd0bbb560b0b4a9
Ethan Hassett <erhassettbg@gmail.com>
2020-05-07 14:20:55 -0400

    v3.1.0 - Fix Webpacker (#5)
    This commit fixes issues with node/yarn that cause webpack to not work correctly.

--- 3.1.0/script/run.sh
+++ 3.1.0/script/run.sh
@@ -3,8 +3,15 @@
 ruby -r erb -e 'puts ERB.new(File.read("/etc/nginx/default.conf.erb")).result' > /etc/nginx/sites-enabled/default.conf
 ruby -r erb -e 'puts ERB.new(File.read("/etc/nginx/passenger.conf.erb")).result' > /etc/nginx/passenger.conf
 
+# Install yarn and compile webpack assets
+yarn install
+bundle exec rails webpacker:install
+bundle exec rails webpacker:compile
+
+# Compile assets in rails pipeline
 rails assets:precompile
 
+# Check for database and migrate or setup the database
 rake db:exists && rake db:migrate || rake db:setup
 
 nginx
